40824734a4076afe6190c9ecb65f0b26fa773eab
erik-ferreira <erikdesouzagf@gmail.com>
2023-09-19 12:07:53 -0300

    save and fetch data

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react"
+import { MMKV } from "react-native-mmkv"
+import { Button, Text, TextInput, View } from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { styles } from "./styles"
+
+interface User {
+  name: string
+  email: string
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const storage = new MMKV({ id: "mmkv-app" })
+
+export default function App() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  const [user, setUser] = useState<User | null>(null)
+
+  function handleSave() {
+    storage.set("user", JSON.stringify({ name, email }))
+
+    fetchUser()
+  }
+
+  function fetchUser() {
+    const data = storage.getString("user")
+    setUser(data ? JSON.parse(data) : null)
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Nome..."
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        placeholder="E-mail..."
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      <Button title="Salvar" onPress={handleSave} />
+
+      <Text style={styles.text}>
+        {user?.name} - {user?.email}
+      </Text>
+    </View>
+  )
+}
---
+++ styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 32,
+    gap: 16,
+  },
+
+  input: {
+    height: 56,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#333",
+    fontSize: 18,
+  },
+
+  text: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+})
